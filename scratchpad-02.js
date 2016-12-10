b02c1544f81a96e9b800331dc85a3caea9627225
baursak <baursak@gmail.com>
2016-12-10 17:10:29 +0300

    lesson 02: switch statement

---
+++ scratchpad-02.js
@@ -0,0 +1,15 @@
+var sign = prompt('what is your astrological sign');
+switch (sign) {
+  case 'taurus':
+    alert('you said taurus');
+    break;
+  case 'virgo':
+    alert('you said virgo');
+    break;
+  case 'leo':
+    alert('you said leo');
+    break;
+  default:
+    alert('you said something unexpected');
+    break;
+}
